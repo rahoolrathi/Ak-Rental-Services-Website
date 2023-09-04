a43ddba85100b362aec0a77489161df1974dd2e2
kanteshkumar813@gmail.com <kanteshkumar813@gmail.com>
2023-09-04 21:46:41 +0500

    NAVBAR

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Fragment } from "react";
+import Home from "./Components/pages/home/Home";
 
 function App() {
   return (
     <Fragment>
-      
+      <Home/>
     </Fragment>
   );
 }
