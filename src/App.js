950b86543612ae5eec702b54efec3c7dd7ce7e3c
kanteshkumar813@gmail.com <kanteshkumar813@gmail.com>
2023-09-04 21:30:03 +0500

    Addition

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Fragment } from "react";
 
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      
+    </Fragment>
+  );
 }
 
 export default App;
