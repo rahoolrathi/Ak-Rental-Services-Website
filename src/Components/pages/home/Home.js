6bc01eb245d4ac5ff79537176151a5a32770bd43
kanteshkumar813@gmail.com <kanteshkumar813@gmail.com>
2023-09-30 02:00:39 +0500

    Car_Card added

--- src/Components/pages/home/Home.js
+++ src/Components/pages/home/Home.js
@@ -1,13 +1,14 @@
 import Navbar from "../../UI/Navbar";
 import ShowCars from "../../UI/ShowCars";
 import HomeInfo from "../../UI/Home_Page_info"; // Adjusted component name to camelCase
-
+import Car_Card from '../Search_Car/SC_UI/Car_Card';
 const Home = () => {
   return (
     <>
       <Navbar />
-      <ShowCars />
-      <HomeInfo /> 
+      <Car_Card/>
+      {/* <ShowCars /> */}
+      {/* <HomeInfo />  */}
     </>
   );
 };
